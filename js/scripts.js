3cc29672eba9186a0ec81b00f377dcaad2fabdb1
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-19 20:00:38 -0700

    Create second test for Pizza.addSize().  Make it pass.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,14 @@ function Pizza(pizzaSize, pizzaCrust, pizzaToppings, pizzaPrice) {
 
 Pizza.prototype.addSize = function(newSize) {
   this.pizzaSize = newSize;
-}
+  if (this.pizzaSize == "small") {
+    this.pizzaPrice += 5.00;
+  } else if (this.pizzaSize == "medium") {
+    this.pizzaPrice += 6.50;
+  } else if (this.pizzaSize == "large") {
+    this.pizzaPrice += 8.00;
+  }
+};
 
 
 // jQuery functions :
--- spec/specs.js
+++ spec/specs.js
@@ -4,18 +4,26 @@ describe("Pizza", function() {
 
   it("creates a pizza with the specified properties.", function() {
     var testPizza = new Pizza("large", "deep dish",
-      "['pepperoni', 'black olives']", "$10.00");
+      "['pepperoni', 'black olives']", 10.00);
     expect(testPizza.pizzaSize).to.equal("large");
     expect(testPizza.pizzaCrust).to.equal("deep dish");
     expect(testPizza.pizzaToppings).to.equal("['pepperoni', 'black olives']");
-    expect(testPizza.pizzaPrice).to.equal("$10.00");
+    expect(testPizza.pizzaPrice).to.equal(10.00);
   });
 
   it("adds a size to the pizza.", function() {
     var testPizza = new Pizza(" ", " ",
-      "[' ', ' ']", "$0.00");
+      "[' ', ' ']", 0.00);
     testPizza.addSize("large");
     expect(testPizza.pizzaSize).to.equal("large");
+  });
+
+  it("changes price when you add a size.", function() {
+    var testPizza = new Pizza(" ", " ",
+      "[' ', ' ']", 0.00);
+    testPizza.addSize("large");
+    expect(testPizza.pizzaSize).to.equal("large");
+    expect(testPizza.pizzaPrice).to.equal(8.00);
   });
 
 });
