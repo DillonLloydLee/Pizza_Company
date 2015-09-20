8476e95130b38f3e83dbea86f5d45b1645d9505a
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-19 22:01:58 -0700

    Add some css styling.  JQuery clicking doesn't work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Pizza prototype :
+// Pizza prototype constructor :
 
 function Pizza(pizzaSize, pizzaCrust, pizzaToppings, pizzaPrice) {
   this.pizzaSize = pizzaSize;
@@ -6,6 +6,9 @@ function Pizza(pizzaSize, pizzaCrust, pizzaToppings, pizzaPrice) {
   this.pizzaToppings = pizzaToppings;
   this.pizzaPrice = pizzaPrice;
 }
+
+
+// Pizza prototype, adding a size :
 
 Pizza.prototype.addSize = function(newSize) {
   if (this.pizzaSize == "small") {
@@ -23,7 +26,10 @@ Pizza.prototype.addSize = function(newSize) {
   } else if (this.pizzaSize == "large") {
     this.pizzaPrice += 8.00;
   }
-};
+}
+
+
+// Pizza prototype, adding a crust :
 
 Pizza.prototype.addCrust = function(newCrust) {
   if (this.pizzaCrust == "deep dish") {
@@ -39,26 +45,35 @@ Pizza.prototype.addCrust = function(newCrust) {
   } else if (this.pizzaCrust == "stuffed") {
     this.pizzaPrice += 2.00;
   }
-};
+}
+
+
+// Pizza prototype, adding a topping :
 
 Pizza.prototype.addTopping = function(newTopping) {
   this.pizzaToppings.push(newTopping);
   this.pizzaPrice += 1.00;
-};
+}
+
+
+// Pizza prototype, removing a topping :
 
 Pizza.prototype.removeTopping = function(oldTopping) {
   var index = this.pizzaToppings.indexOf(oldTopping);
   this.pizzaToppings.splice(index, 1);
   this.pizzaPrice -= 1.00;
-};
+}
 
 
-// Order prototype :
+// Order prototype constructor :
 
 function Order(totalFood, totalPrice) {
   this.totalFood = totalFood;
   this.totalPrice = totalPrice;
 }
+
+
+// Order prototype, adding an item :
 
 Order.prototype.addItem = function(newItem) {
   if (newItem == "2 liter of Coca-Cola") {
@@ -84,16 +99,89 @@ Order.prototype.addItem = function(newItem) {
   }
 }
 
+
+// Order prototype, starting over :
+
 Order.prototype.startOver = function() {
   this.totalFood = [];
   this.totalPrice = 0.00;
 }
 
 
+// Starting an initial order :
+
+var order = new Order([], 0.00);
+
 
 // jQuery functions :
 
 $(document).ready(function() {
-  $(".first-page").addClass('animated rollIn');
+  $(".container").addClass('animated rollIn');
+
+  $(".go-to-order").click(function() {
+    $(".initial-page").toggle;
+    $(".choose-page").toggle;
+  });
+
+  $(".go-to-about").click(function() {
+    $(".initial-page").toggle;
+    $(".about-page").toggle;
+  });
+
+  $(".go-to-initial").click(function() {
+    $(".about-page").toggle;
+    $(".initial-page").toggle;
+  });
+
+  $(".go-to-pizzas").click(function() {
+    $(".choose-page").toggle;
+    $(".pizza-page").toggle;
+    var thisPizza = new Pizza(" ", " ", [], 0.00);
+  });
+
+  $(".go-to-sides").click(function() {
+    $(".choose-page").toggle;
+    $(".side-page").toggle;
+  });
+
+  $(".go-to-sodas").click(function() {
+    $(".choose-page").toggle;
+    $(".soda-page").toggle;
+  });
+
+  $(".go-to-confirm-from-pizzas").click(function() {
+    $(".pizza-page").toggle;
+    $(".confirm-page").toggle;
+  });
+
+  $(".go-to-confirm-from-sides").click(function() {
+    $(".side-page").toggle;
+    $(".confirm-page").toggle;
+  });
+
+  $(".go-to-confirm-from-sodas").click(function() {
+    $(".soda-page").toggle;
+    $(".confirm-page").toggle;
+  });
+
+  $(".go-to-final").click(function() {
+    $(".confirm-page").toggle;
+    $(".final-page").toggle;
+  });
+
+  $(".go-back-to-pizzas").click(function() {
+    $(".confirm-page").toggle;
+    $(".pizza-page").toggle;
+  });
+
+  $(".go-back-to-sides").click(function() {
+    $(".confirm-page").toggle;
+    $(".side-page").toggle;
+  });
+
+  $(".go-back-to-sodas").click(function() {
+    $(".confirm-page").toggle;
+    $(".soda-page").toggle;
+  });
 
 });
